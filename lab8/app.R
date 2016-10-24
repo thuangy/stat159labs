062be8335e873535223d842336544f1b99e451fa
Tina Huang <thuang@berkeley.edu>
2016-10-24 12:51:45 -0700

    done with basic shinydoc and readme

--- lab8/app.R
+++ lab8/app.R
@@ -4,37 +4,28 @@ palette(c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3",
   "#FF7F00", "#FFFF33", "#A65628", "#F781BF", "#999999"))
 
 library(shiny)
+advertising = read.csv("advertising.csv")
 
 ui <- fluidPage(
-  headerPanel('Iris k-means clustering'),
+  headerPanel('Advertising Plots of Sales Vs. Specific Media Budgets'),
   sidebarPanel(
-    selectInput('xcol', 'X Variable', names(iris)),
-    selectInput('ycol', 'Y Variable', names(iris),
-      selected = names(iris)[[2]]),
-    numericInput('clusters', 'Cluster count', 3,
-      min = 1, max = 9)
+    selectInput('xcol', 'Media Budget', names(advertising)[2:4])
   ),
   mainPanel(
-    plotOutput('plot1')
+    plotOutput('plot')
   )
 )
 
 server <- function(input, output) {
 
   selectedData <- reactive({
-    iris[, c(input$xcol, input$ycol)]
+    advertising[, c(input$xcol, "Sales")]
   })
 
-  clusters <- reactive({
-    kmeans(selectedData(), input$clusters)
-  })
-
-  output$plot1 <- renderPlot({
+  output$plot <- renderPlot({
     par(mar = c(5.1, 4.1, 0, 1))
     plot(selectedData(),
-         col = clusters()$cluster,
-         pch = 20, cex = 3)
-    points(clusters()$centers, pch = 4, cex = 4, lwd = 4)
+         pch = 20, cex = 2)
   })
 
 }
